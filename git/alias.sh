140d8d1573cd07b420cbe0301cf0504bdb079491
Erik Hallaraaker <erikhallaraaker@gmail.com>
2021-10-19 10:54:37 +0200

    add git alias

---
+++ git/alias.sh
@@ -0,0 +1,5 @@
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.cm "commit -m"
+git config --global alias.st status
